00ae4427c71e7e5c159db8ea7254187bcc05be73
C-Tonia <ogunwatonia01@gmail.com>
2023-06-21 08:49:41 +0100

    fetch the countries from the api

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-const toggleDarkModeEl = document.querySelector("body");
+const global = {
+  currentPage: window.location.pathname,
+  isDarkMode: false,
+};
+console.log(window.location.pathname);
+const toggleDarkModeEl = document.querySelector(".btn-toggle");
+const bodyEl = document.querySelector("body");
 
 toggleDarkModeEl.addEventListener("click", function () {
   const moonIcon = document.querySelector("i");
+  global.isDarkMode = bodyEl.classList.toggle("dark-mode");
 
-  toggleDarkModeEl.classList.toggle("dark-mode");
-
-  if (moonIcon.classList.contains("bi-moon")) {
+  if (global.isDarkMode) {
     moonIcon.classList.remove("bi-moon");
     moonIcon.classList.add("bi-moon-fill");
   } else {
@@ -14,5 +19,64 @@ toggleDarkModeEl.addEventListener("click", function () {
   }
 });
 
+async function displayAllCountry() {
+  const results = await fetchData("all");
+  console.log(results);
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="detail.html?name=${result.name.common}">
+    <div class="card-header">
+      <img src="${result.flags.png}" alt="${result.flags.alt}" />
+    </div>
+    <div class="card-body">
+      <h2>${result.name.common}</h2>
+      <p>
+        <span class="bold">Population:</span>
+        <span class="value">${result.population}</span>
+      </p>
+      <p>
+        <span class="bold">Region:</span>
+        <span class="value">${result.region}</span>
+      </p>
+      <p>
+        <span class="bold">Capital:</span>
+        <span class="value">${result.capital}</span>
+      </p>
+    </div>
+  </a>
+    `;
+    document.querySelector("#all-country").appendChild(div);
+  });
+}
+
+// Fetch data from the local json
+async function fetchData(endpoint) {
+  //   showSpinner();
+  const response = await fetch(`https://restcountries.com/v3.1/${endpoint}`);
+
+  const data = await response.json();
+  //   hideSpinner();
+  return data;
+}
+
+// function showSpinner() {
+//   document.querySelector(".spinner").classList.add("show");
+// }
+// function hideSpinner() {
+//   document.querySelector(".spinner").classList.remove("show");
+// }
 // Route
-function init() {}
+async function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      displayAllCountry();
+      break;
+    case `/detail.html`:
+      //   detailedPage();
+      break;
+  }
+}
+init();
